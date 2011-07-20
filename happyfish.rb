6b1e8b367679ee19dbff81142160342b317e2e72
Yuanxuan Wang <zellux@gmail.com>
2011-07-20 17:44:22 +0800

    Use multithreading to pick up money

--- happyfish.rb
+++ happyfish.rb
@@ -22,23 +22,24 @@ class HappyFishBot
     }
 
     @log = Logger.new(STDOUT)
+    @mutex = Mutex.new
   end
   
   def signin
     if not @config['session']
-      @log.info('Logging into sina weibo ...')
+      @log.info 'Logging into sina weibo ...'
       login(@agent, @config['username'], @config['password'])
       html = @agent.get('http://game.weibo.com/happyisland?origin=1026').body
       url = html[/iframe\s+src="(.*?)"/, 1]
       @config['session'] ||= url
     end
     
-    @log.info('Logging into happy fish game ...')
-    html = @agent.get(url).body
+    @log.info 'Logging into happy fish game ...'
+    html = @agent.get(@config['session']).body
   end
 
   def reload
-    @log.info('Loading user data ...')
+    @log.info 'Loading user data ...'
     req = @agent.post("http://wbisland.hapyfish.com/api/inituserinfo", "first" => "1")
     @user_info = JSON.parse(req.body)
     req = @agent.post("http://wbisland.hapyfish.com/api/initisland?ts=#{Time.now.to_i}050", "ownerUid" => @user_info['user']['uid'])
@@ -46,21 +47,26 @@ class HappyFishBot
   end
 
   def pick_own_money()
-    @log.info('Picking up own money ...')
+    @log.info 'Picking up own money ...'
     expsum = coinsum = 0
-    @island_info['islandVo']['buildings'].select{|x|x['deposit']}.each do |item|
-      req = @agent.post("http://wbisland.hapyfish.com/api/harvestplant?ts=#{Time.now.to_i}050", "itemId" => item["id"])
-      response = JSON.parse(req.body)
-      exp = response['expChange'].to_i rescue 0
-      coin = response['coinChange'].to_i rescue 0
-      expsum += exp
-      coinsum += coin
+    @island_info['islandVo']['buildings'].select{|x| x['deposit'] }.each do |item|
+      Thread.new {
+        req = @agent.post("http://wbisland.hapyfish.com/api/harvestplant?ts=#{Time.now.to_i}050", "itemId" => item["id"])
+        response = JSON.parse(req.body)
+        exp = response['expChange'].to_i rescue 0
+        coin = response['coinChange'].to_i rescue 0
+        @mutex.synchronize {
+          expsum += exp
+          coinsum += coin
+        }
+      }.join
+      
     end
-    puts "Received #{expsum} EXP, #{coinsum} coins"
+    @log.info "Received #{expsum} EXP, #{coinsum} coins"
   end
   
 end
-
+  
 bot = HappyFishBot.new
 bot.signin
 bot.reload
