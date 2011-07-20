e6817e88319f6626efc653595f22d36c067433ce
Yuanxuan Wang <zellux@gmail.com>
2011-07-20 16:58:19 +0800

    Refactoring: use class

--- happyfish.rb
+++ happyfish.rb
@@ -6,41 +6,42 @@ require 'sina'
 require 'logger'
 require 'json'
 
-$LOG = Logger.new(STDOUT)
+class HappyFishBot
+  def initialize
+    @config = YAML::load_file('config.yml') rescue {}
+    @config['username'] ||= ask('Please enter your username: ')
+    @config['password'] ||= ask('Please enter your password: ') {|q| q.echo = false}
+    at_exit {
+      File.open('config.yml', 'w') do |out|
+        YAML::dump(@config, out)
+      end
+    }
 
-def prompt_and_get(hint)
-  STDOUT.write(hint)
-  gets
-end
-  
-config = YAML::load_file('config.yml') rescue {}
-config['username'] ||= ask('Please enter your username: ')
-config['password'] ||= ask('Please enter your password: ') {|q| q.echo = false}
+    @agent = Mechanize.new { |agent|
+      agent.user_agent_alias = 'Mac Safari'
+    }
 
-at_exit {
-  File.open('config.yml', 'w') do |out|
-    YAML::dump(config, out)
+    @log = Logger.new(STDOUT)
   end
-}
+  
+  def signin
+    @log.info('Logging into sina weibo...')
+    login(@agent, @config['username'], @config['password'])
+    
+    @log.info('Logging into happy fish game...')
+    html = @agent.get('http://game.weibo.com/happyisland?origin=1026').body
+    url = html[/iframe\s+src="(.*?)"/, 1]
+    html = @agent.get(url).body
+  end
 
-$agent = Mechanize.new { |agent|
-  agent.user_agent_alias = 'Mac Safari'
-}
+  def reload
+    req = @agent.post("http://wbisland.hapyfish.com/api/inituserinfo", "first" => "1")
+    @userinfo = JSON.parse(req.body)
+    puts @userinfo
+  end
+end
 
-$LOG.info('Logging into sina weibo...')
-login($agent, config['username'], config['password'])
+bot = HappyFishBot.new
+bot.signin
+bot.reload
 
-$LOG.info('Getting happy fish game url...')
-html = $agent.get('http://game.weibo.com/happyisland?origin=1026').body
-
-url = html[/iframe\s+src="(.*?)"/, 1]
-$LOG.info("Loading #{url}...")
-html = $agent.get(url).body
-
-req = $agent.post("http://wbisland.hapyfish.com/api/inituserinfo", "first" => "1")
-json = JSON.parse(req.body)
-
-puts json
-
-# req = $agent.post("http://wbisland.hapyfish.com/api/initisland?ts=#{Time.now.to_i}000",
-#             '
