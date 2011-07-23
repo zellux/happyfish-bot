f33d80e5dc2d35171ce8b3ce3820d3a4661d43d7
Yuanxuan Wang <zellux@gmail.com>
2011-07-23 14:58:26 +0800

    Dump scheduler list before exit

--- console.rb
+++ console.rb
@@ -16,17 +16,18 @@ at_exit {
   File.open(config_file, 'w') do |out|
     YAML::dump(bot.config, out)
   end
+  @scheduler.dump_events
 }
 
 bot.signin
 bot.reload
 
-scheduler = bot.scheduler
-scheduler.add_event(Time.now, bot.method(:refresh_data), "Refresh data")
-scheduler.add_event(Time.now + BUILDING_REPAIR_INTERVAL, bot.method(:building_check), "Repair all buildings")
-scheduler.dump_events()
+@scheduler = bot.scheduler
+@scheduler.add_event(Time.now, bot.method(:refresh_data), "Refresh data")
+@scheduler.add_event(Time.now + BUILDING_REPAIR_INTERVAL, bot.method(:building_check), "Repair all buildings")
+@scheduler.dump_events
 
 while true
   sleep(1)
-  scheduler.do_anything
+  @scheduler.do_anything
 end
